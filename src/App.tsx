a0988dfa8cf97ce235dce9038d04e4e0c9579ca4
Scott Clingan <sclingan1@gmail.com>
2023-04-20 15:32:10 -0500

    Added mobile layout and SCSS variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
+import star from '../public/images/icon-star.svg'
+import anne from '../public/images/image-anne.jpg'
+import colton from '../public/images/image-colton.jpg'
+import irene from '../public/images/image-irene.jpg'
 import './App.css'
 
 function App() {
 
   return (
-    <div>
+    <div className='App'>
       <header>
         <h1>10,000+ of our users love our products.</h1>
         <p>
@@ -13,50 +17,62 @@ function App() {
         </p>
       </header>
       <aside>
+        <div className='rating'>
+          <img src={star} alt="" className='icon-star'></img>
         <p>
           Rated 5 Stars in Reviews
         </p>
+        </div>
+        <div className='rating'>
+        <img src={star} alt="" className='icon-star'></img>
         <p>
           Rated 5 Stars in Report Guru
         </p>
+        </div>
+        <div className='rating'>
+        <img src={star} alt="" className='icon-star'></img>
         <p>
           Rated 5 Stars in BestTech
         </p>
+        </div>
       </aside>
       <main>
-        <div>
+        <div className='profile'>
+          <img src={colton} alt="" className='profile-icon'></img>
         <p>
           Colton Smith 
         </p>
-        <p>
+        <p className='verified'>
           Verified Buyer
         </p>
-        <p>
+        <p className='testimony'>
           "We needed the same printed design as the one we had ordered a week prior.
           Not only did they find the original order, but we also received it in time.
           Excellent!"
         </p>
         </div>
-        <div>
+        <div className='profile'>
+        <img src={irene} alt="" className='profile-icon'></img>
         <p>
           Irene Roberts 
         </p>
-        <p>
+        <p className='verified'>
           Verified Buyer
         </p>
-        <p>
+        <p className='testimony'>
           "Customer service is always excellent and very quick turn around. Completely
           delighted with the simplicity of the purchase and the speed of delivery."
         </p>
         </div>
-        <div>
+        <div className='profile'>
+        <img src={anne} alt="" className='profile-icon'></img>
         <p>
           Anne Wallace 
         </p>
-        <p>
+        <p className='verified'>
           Verified Buyer
         </p>
-        <p>
+        <p className='testimony'>
           "Put an order with this company and can only praise them for the very high
           standard. Will definitely use them again and recommend them to everyone!"
         </p>
